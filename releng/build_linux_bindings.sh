2eae2e43e4708acb964c1c5dcce765005335867e
Peter Chang <peter.chang@diamond.ac.uk>
2026-03-12 13:49:54 +0000

    Change to manylinux 2_28 so remove RHEL 7 or earlier support
    This removes the need of our own image as the new image based on
    AlmaLinux 8 includes EPEL (and we no longer use cross-compilation
    for win32)

--- releng/build_linux_bindings.sh
+++ releng/build_linux_bindings.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e -x
 
-# docker run -it --env="ARCH=x86_64" --env="PLAT_OS=linux" -v $(pwd):/io:Z ghcr.io/diamondlightsource/manylinux-dls-2014_x86_64:latest /bin/bash /io/releng/build_linux_bindings.sh
+# docker run -it --env="ARCH=x86_64" --env="PLAT_OS=linux" -v $(pwd):/io:Z quay.io/pypa/manylinux_2_28_x86_64:latest /bin/bash /io/releng/build_linux_bindings.sh
+
+# install jdk
+dnf install -y java-11-openjdk-devel
 
 if [ -z "$START_DIR" ]; then
     export START_DIR="/io"
